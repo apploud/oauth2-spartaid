7b006b2c55c3e573d7b2650bcad03be04bdd79e2
Jan Konáš <jan.konas@gmail.com>
2019-11-07 13:53:28 +0100

    Add yourpass URL getter

--- src/Provider/SpartaIdResourceOwner.php
+++ src/Provider/SpartaIdResourceOwner.php
@@ -66,6 +66,12 @@ class SpartaIdResourceOwner implements ResourceOwnerInterface
 	}
 
 
+	public function getYourpassUrl(): ?string
+	{
+		return $this->data['yourpass_url'] ?: null;
+	}
+
+
     public function toArray(): array
     {
         return $this->data;
